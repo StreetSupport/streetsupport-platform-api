61ee3afa0de484f858c0cf75ddc55ab8660e4023
Oleksandr Seliuchenko <o.seliuchenko@honeycombsoft.com>
2025-09-03 14:12:39 +0300

    Add staging env for logging

--- instrument.ts
+++ instrument.ts
@@ -5,6 +5,7 @@ dotenv.config();
 
 Sentry.init({
   dsn: process.env.SENTRY_DSN,
+  environment: process.env.SENTRY_ENVIRONMENT,
   // Setting this option to true will send default PII data to Sentry.
   // For example, automatic IP address collection on events
   sendDefaultPii: true,
